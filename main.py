712470fc612ae40a9a63088b64e4e8a3bd6fcc86
UnOriginalThor <tuvesonjoel@gmail.com>
2025-01-14 15:09:16 -0500

    spent too long on trying to make the report work, but it's done and I *kind of* understand how it works

--- main.py
+++ main.py
@@ -2,10 +2,31 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     word_count = get_word_count(text)
-    letter_count = get_letter_count(text)
-    print(letter_count)
+    character_count = get_character_count(text)
+    sort_list = dict_sort(character_count)
 
-def get_letter_count(text):
+    print(f"=== Begin Report of {book_path} ===")
+    print(f"{word_count} words found in the document")
+    print()
+
+    for i in sort_list:
+        if i["letter"].isalpha():
+            print(f"The {i["letter"]} character was found {i["num"]} times")
+
+    print("=== End Report ===")
+
+
+def letter_sort(i):
+    return i["num"]
+
+def dict_sort(text):
+    new_list = []
+    for i in text:
+        new_list.append({"letter": i, "num": text[i]})
+    new_list.sort(reverse=True, key=letter_sort)
+    return new_list
+    
+def get_character_count(text):
     string = {}
     for i in text.lower():
         if i in string:
